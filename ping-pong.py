9eec416b21272ef0355c7fd9e45f4ed3109a3ed8
kapystad <73845870+darik523@users.noreply.github.com>
2021-04-18 10:57:26 +0300

    v  1.1
    • Added new background
    • added paddles and their movement
    
    the ball and its movement will be added soon ☻

--- ping-pong.py
+++ ping-pong.py
@@ -1,22 +1,69 @@
-from pygame import*
+from pygame import *
+from time import time as timer
+font.init()
+#display
+win_width = 600
+win_height = 700
+window = display.set_mode((win_width, win_height))
+display.set_caption('Hello')
+bg = transform.scale(image.load('backdrop.jpg'), (win_width, win_height))
+#display
 
-#игровая сцена
-bg = (34, 153, 153)
-win_wigth = 450
-win_height = 600
-window = display.set_mode((400, 470))
-window.fill(bg)
-display.set_caption('ping-pong')
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
-clock = time.Clock()
-FPS = 60
+class Player(GameSprite):
+    
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_a] and self.rect.x > 10:
+            self.rect.x -= self.speed
+        if keys[K_d] and self.rect.x < 350:
+            self.rect.x += self.speed
+        
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 10:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < 350:
+            self.rect.x += self.speed
+
+
+font1 = font.Font(None, 35)
+lose1 = font1.render('Player 1 lose!', True, (76, 0, 51))
+
+player_1 = Player('platform.png', 5, win_height - 100, 250, 65, 6)
+player_2 = Player('platform.png', 5, win_height - 660, 250, 65, 6)
+ball = Player('ballp.png', 5, win_height - 0, 250, 65, 5)
+
+
 game = True
-
-#игровой цикл:
+finish = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    
+
+
+    if finish != True:
+        window.blit(bg,(0, 0))
+        
+        player_1.reset()
+        player_1.update_l()
+
+        player_2.reset()
+        player_2.update_r()
+
+        ball.reset()
+        
+        
+
     display.update()
-    clock.tick(FPS)
