d7bcba598dee2f912c8edca26f2d2891c59bfc6d
kapystad <73845870+darik523@users.noreply.github.com>
2021-04-18 11:31:59 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -36,14 +36,19 @@ class Player(GameSprite):
         if keys[K_RIGHT] and self.rect.x < 350:
             self.rect.x += self.speed
 
-
+font.init()
 font1 = font.Font(None, 35)
 lose1 = font1.render('Player 1 lose!', True, (76, 0, 51))
+lose2 = font1.render('Player 2 lose!', True, (76, 0, 51))
+
+
 
 player_1 = Player('platform.png', 5, win_height - 100, 250, 65, 6)
 player_2 = Player('platform.png', 5, win_height - 660, 250, 65, 6)
-ball = Player('ballp.png', 5, win_height - 0, 250, 65, 5)
+ball = GameSprite('ballp.png', 5, win_height - 0, 250, 65, 5)
 
+speed_x = 3
+speed_y = 3
 
 game = True
 finish = False
@@ -63,6 +68,40 @@ while game:
         player_2.update_r()
 
         ball.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(player_1, ball) or sprite.collide_rect(player_2, ball):
+            speed_x *= -1
+            speed_y *= 1
+
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if ball .rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
         
 
